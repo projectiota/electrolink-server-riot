01826bbaf6dd6d7117b5438acec7e325b1f37dc6
Paul RATHGEB <paul.rathgeb@skynet.be>
2017-01-29 13:45:07 +0100

    Indentation error ....
    Signed-off-by: Paul RATHGEB <paul.rathgeb@skynet.be>

--- tools/testing/test_from_weio.py
+++ tools/testing/test_from_weio.py
@@ -10,58 +10,58 @@ GPIO_OUT = 3
 connected = False
 while not connected:
     try:
-        ser = serial.Serial("/dev/ttyACM0", baudrate="115200");
+        ser = serial.Serial("/dev/ttyACM0", baudrate="115200", timeout=1);
         print ser
         connected = True
     except:
         time.sleep(0.1)
 
-    while True:
-        string = '{ "method": "pinMode", "params": [%d, %d] }'%(46, GPIO_OUT)
+while True:
+    string = '{ "method": "inMode", "params": [%d, %d] }'%(46, GPIO_OUT)
 
-        ser.write(string)
-        size = 0
-        while not size:
-            size = ser.inWaiting()
-
-        a = json.loads(ser.read(size))
-        print a["Status"]
-
-        string = '{ "method": "pinMode", "params": [%d, %d] }'%(9, GPIO_IN_PU)
-
-        ser.write(string)
-        size = 0
-        while not size:
-            size = ser.inWaiting()
-        a = json.loads(ser.read(size))
-        print a["Status"]
-
-        string = '{ "method": "digitalRead", "params": [%d] }'%(9)
-
-        ser.write(string)
-        size = 0
-        while not size:
-            size = ser.inWaiting()
-        a = json.loads(ser.read(size))
-        print a["value"]
-
-        string = '{ "method": "digitalWrite", "params": [%d, %d] }'%(46, 1)
-        ser.write(string)
-        size = 0
-            while not size:
+    ser.write(string)
+    size = 0
+    while not size:
         size = ser.inWaiting()
-        a = json.loads(ser.read(size))
-        print a["Status"]
 
-        #time.sleep(0.1)
+    a = json.loads(ser.read(size))
+    print a["Status"]
 
-        string = '{ "method": "digitalWrite", "params": [%d, %d] }'%(46, 0)
-        ser.write(string)
-        size = 0
-        while not size:
-            size = ser.inWaiting()
-        a = json.loads(ser.read(size))
-        print a["Status"]
+    string = '{ "method": "pinMode", "params": [%d, %d] }'%(9, GPIO_IN_PU)
 
-        #time.sleep(0.1)
+    ser.write(string)
+    size = 0
+    while not size:
+        size = ser.inWaiting()
+    a = json.loads(ser.read(size))
+    print a["Status"]
+
+    string = '{ "method": "digitalRead", "params": [%d] }'%(9)
+
+    ser.write(string)
+    size = 0
+    while not size:
+        size = ser.inWaiting()
+    a = json.loads(ser.read(size))
+    print a["value"]
+
+    string = '{ "method": "digitalWrite", "params": [%d, %d] }'%(46, 1)
+    ser.write(string)
+    size = 0
+    while not size:
+        size = ser.inWaiting()
+    a = json.loads(ser.read(size))
+    print a["Status"]
+
+    #time.sleep(0.1)
+
+    string = '{ "method": "digitalWrite", "params": [%d, %d] }'%(46, 0)
+    ser.write(string)
+    size = 0
+    while not size:
+        size = ser.inWaiting()
+    a = json.loads(ser.read(size))
+    print a["Status"]
+
+    #time.sleep(0.1)
 
