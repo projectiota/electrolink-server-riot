33664b71fd4b343b29b22972d25afbaefcc4cdf1
Paul RATHGEB <paul.rathgeb@skynet.be>
2017-01-29 12:39:05 +0100

    Updated my testing script (for backup)
    Signed-off-by: Paul RATHGEB <paul.rathgeb@skynet.be>

--- tools/testing/test_from_weio.py
+++ tools/testing/test_from_weio.py
@@ -1,10 +1,67 @@
 import serial
 import time
+import json
 
-ser = serial.Serial("/dev/ttyACM0", baudrate="115200");
-print ser
+GPIO_IN = 0
+GPIO_IN_PD = 1
+GPIO_IN_PU = 2
+GPIO_OUT = 3
 
-while True:
-	time.sleep(.0001)
-	string = '{ "method": "digitalWrite", "params": [1, 2] }'
-	ser.write(string)
+connected = False
+while not connected:
+    try:
+        ser = serial.Serial("/dev/ttyACM0", baudrate="115200");
+        print ser
+        connected = True
+    except:
+        time.sleep(0.1)
+
+    while True:
+        string = '{ "method": "pinMode", "params": [%d, %d] }'%(46, GPIO_OUT)
+
+        ser.write(string)
+        size = 0
+        while not size:
+            size = ser.inWaiting()
+
+        a = json.loads(ser.read(size))
+        print a["Status"]
+
+        string = '{ "method": "pinMode", "params": [%d, %d] }'%(9, GPIO_IN_PU)
+
+        ser.write(string)
+        size = 0
+        while not size:
+            size = ser.inWaiting()
+        a = json.loads(ser.read(size))
+        print a["Status"]
+
+        string = '{ "method": "digitalRead", "params": [%d] }'%(9)
+
+        ser.write(string)
+        size = 0
+        while not size:
+            size = ser.inWaiting()
+        a = json.loads(ser.read(size))
+        print a["value"]
+
+        string = '{ "method": "digitalWrite", "params": [%d, %d] }'%(46, 1)
+        ser.write(string)
+        size = 0
+            while not size:
+        size = ser.inWaiting()
+        a = json.loads(ser.read(size))
+        print a["Status"]
+
+        #time.sleep(0.1)
+
+        string = '{ "method": "digitalWrite", "params": [%d, %d] }'%(46, 0)
+        ser.write(string)
+        size = 0
+        while not size:
+            size = ser.inWaiting()
+        a = json.loads(ser.read(size))
+        print a["Status"]
+
+        #time.sleep(0.1)
+
